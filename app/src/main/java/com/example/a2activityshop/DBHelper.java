4acd312564472a2bce3207a932655bb76f7cea02
Lolipopec <pacha_vlasov_95@mail.ru>
2021-11-04 19:30:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a2activityshop/DBHelper.java
@@ -0,0 +1,51 @@
+package com.example.a2activityshop;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+public class DBHelper  extends SQLiteOpenHelper{
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "catalogDb";
+    public static final String TABLE_CATALOG = "catalog";
+
+    public static final String KEY_ID = "_id";
+    public static final String KEY_PRODUCT = "product";
+    public static final String KEY_PRICE = "price";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + TABLE_CATALOG + "(" + KEY_ID
+                + " integer primary key," + KEY_PRODUCT + " text," + KEY_PRICE + " text"+")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_CATALOG);
+
+        onCreate(db);
+
+    }
+}
